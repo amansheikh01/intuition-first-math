980af005493d2a2d9d39e4c9368e5db0c8709175
amansheikh01 <amansheikh01@users.noreply.github.com>
2026-02-11 12:15:03 +0530

    Update next.config.js to match user specification

--- next.config.js
+++ next.config.js
@@ -1,13 +1,8 @@
-/** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  // GitHub Pages hosting requirement
   basePath: '/intuition-first-math',
   assetPrefix: '/intuition-first-math/',
-  trailingSlash: true,
-  images: {
-    unoptimized: true, // Image optimization requires a server
-  },
+  images: { unoptimized: true },
 };
 
 module.exports = nextConfig;
